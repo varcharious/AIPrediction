fe3b39b0feb1006cd6b154e4697cb8bf7af85c61
varcharious <varcharious@users.noreply.github.com>
2024-03-28 01:50:06 +0000

    Code updates

--- Survey.cpp
+++ Survey.cpp
@@ -31,190 +31,235 @@ double bayesTheorem(double priorProbability, double likelihood, double evidence)
     return ((likelihood * priorProbability) / evidence);
 }
 
+bool isInputInt(const std::string& input, int& res) {
+
+    try {
+        res = std::stoi(input);
+
+        if(res >= 1 && res <= 4) {
+            return true;
+        }
+    } catch (...) {
+
+    }
+    return false;
+
+}
+
+// convert string from file into a double
+double stringToDouble(const std::string& input) {
+    try {
+        return std::stod(input);
+    } catch (...) {
+        return 0.25;
+    }
+}
+
+// first let's create all the files we need
+void createFiles() {
+
+    for(int i = 0; i < affiliations.size(); ++i) {
+
+        // file doesn't exist, so create it
+        std::ofstream newFile(affiliations[i] + ".txt");
+        if (newFile.is_open()) {
+
+            // inital weight is 0.25
+            newFile << "0.25";
+            std::cout << "New file was created!" << std::endl;
+            newFile.close();
+
+        } else {
+
+            std::cerr << "Oops, something went wrong creating the file.";
+            return;
+
+        }  
+
+    }
+
+    
+
+}
+
 int main() {   
 
+    // create all the files
+    createFiles();
+
     // create a string that will hold user input of political affiliation
-    std::string userPoliticalParty; 
+    std::string userInputPoliticalParty; 
     std::string haveAsked;
     
+    // loop through all questions
     for (int i = 0; i < QuestionCount; ++i) {
 
         // ask the question
         std::cout << "Question " << i + 1 << ": " << surveyData[i][0] << std::endl;
         for (int q = 1; q < AnswerCount + 1; ++q) {
-            std::cout << "    Answer " << q << ": " << surveyData[i][q] << std::endl;
+            std::cout << "    " << q << ": " << surveyData[i][q] << std::endl;
         }
         std::cout << std::endl;
 
         // get the input
-        int userInput;
-        std::cout << "Type the number of your choice:";
+        std::string userInput;
+        std::cout << "Type the number of your choice and press enter:" << std::endl;
         std::cin >> userInput;
 
-        // make sure user input is in range
-        if(userInput >= 1 && userInput <= AnswerCount) {                    
-            
-            // figure out the affiliation associated with the answer given
-            std::string questionAffiliation;
-            for(int z = 1; z < affiliations.size() + 1; ++z) {
+        // validate user input
+        int number;
+        while(isInputInt(userInput, number) != true) {
+            std::cout << "Oops, your answer needs to be a number character 1-4." << std::endl;
+            std::cout << "Type the number of your choice and press enter:" << std::endl;
+            std::cin >> userInput;
+        }
 
-                if(userInput == z) {
-                    questionAffiliation = affiliations[z - 1];
-                }
+        std::cout << "Thank you, let's continue." << std::endl;
+        
+        // figure out the affiliation associated with the answer given
+        std::string questionAffiliation;
+        for(int z = 1; z < affiliations.size() + 1; ++z) {
 
+            if(number == z) {
+                questionAffiliation = affiliations[z - 1];
             }
 
-            // check if file exists
-            std::string fileContent;
-            std::ifstream file(questionAffiliation + ".txt");
-            if (!file.is_open()) {
+        }
 
-                // file doesn't exist, so create it
-                std::ofstream newFile(questionAffiliation + ".txt");
-                if (newFile.is_open()) {
+        // check if file exists
+        std::string fileContent;
+        std::ifstream file(questionAffiliation + ".txt");
+        if (!file.is_open()) {
 
-                    // inital weight is 0.25
-                    newFile << "0.25";
-                    std::cout << "New file was created!";
-                    newFile.close();
-
-                } else {
-
-                    std::cerr << "Oops, something went wrong creating the file.";
-                    return 1;
-
-                }       
-
-            } else {
-
-                //std::filesystem::path filePath = std::filesystem::absolute(questionAffiliation + ".txt");
-                //std::cout << "File location: " << filePath << std::endl;
-
-                // set fileContent to contents of the file
-                std::getline(file, fileContent);
-
-                // close file
-                file.close();
-
-                // get the prior probability
-                double priorProbability;
-                priorProbability = std::stod(fileContent);
-                
-                // did we get any file content?
-                if (priorProbability < 0) {
-                    std::cerr << "Error reading file: " << questionAffiliation << ".txt" << std::endl;
-                    return 1;
-                } else {                    
-                    
-                    // let's play with Bayes' Theorem                    
-                    double likelihood = 0.8;
-                    double evidence = (likelihood * priorProbability) + ((1 - likelihood) * ( 1 - priorProbability));
-                    double posteriorProbability = bayesTheorem(priorProbability, likelihood, evidence);
-
-                    // reopen the file for writing
-                    std::ofstream outputfile(questionAffiliation + ".txt");
-
-                    // verify the file is open
-                    if (!outputfile.is_open()) {
-                        std::cerr << "Error reopening file";
-                        return 1;
-                    }
-
-                    // write back to the file
-                    outputfile << std::to_string(posteriorProbability) << std::endl;
-
-                    // close the file
-                    outputfile.close();
-
-                    // we are comfortable enough to make a prediction
-                    if(posteriorProbability > .8 && i > 3) {
-
-                        // if we already asked... don't ask again
-                        if(haveAsked != "true") {
-
-                            // we need to obtain the user's affiliation
-                            std::cout << "What number corresponds to your political affiliation?" << std::endl;
-                            std::cout << "1: Democrat" << std::endl;
-                            std::cout << "2: Republican" << std::endl;
-                            std::cout << "3: Green Party" << std::endl;
-                            std::cout << "4: Independent" << std::endl;
-                            std::cin >> userPoliticalParty;
-
-                            if(std::stod(userPoliticalParty) > 0 && std::stod(userPoliticalParty) < 5) {
-                                haveAsked = "true";
-                            } else {
-                                std::cout << "Oops... your answer needs to be between 1 and 5: " << std::endl;
-                                std::cin >> userPoliticalParty;
-                            }
-                            
-
-                        }       
-
-                        // convert user's number answer into the text equiv
-                        std::string affiliationMatch = affiliations[std::stod(userPoliticalParty) - 1];
-
-                        // were we successful? if so... make prediction, If not... keep learing
-                        if (questionAffiliation == affiliationMatch) {
-                            
-                            std::cout << "Political Affiliation Prediction: " << questionAffiliation << std::endl;
-                            return 0;
-
-                        } else if (i == QuestionCount - 1) { // are we out of questions to ask?
-
-                            std::string predAffiliation = "";
-                            double prevProbability = 0;
-
-                            // if we didn't predict correctly, make the best guess we can
-                            for (int n = 0; n < affiliations.size(); ++n) {
-
-                                // open file
-                                std::string fContent;
-                                std::ifstream affiliationFile(affiliations[n] + ".txt");
-                                if (!affiliationFile.is_open()) {
-                                    std::cerr << "Error opening file";
-                                    return 1;
-                                } else {
-
-                                    // get posterior probability
-                                    std::getline(affiliationFile, fContent);
-
-                                    if (n==0) {
-                                        predAffiliation = questionAffiliation;
-                                        prevProbability = std::stod(fContent);
-                                    } else {
-                                        
-                                        if (std::stod(fContent) > prevProbability) {
-                                            // update the most li
-                                            predAffiliation = questionAffiliation;
-                                        }
-                                    }
-
-                                    // close file
-                                    affiliationFile.close();
-
-                                }
-
-                            }
-
-                            // make our best guess
-                            std::cout << "Political Affiliation Prediction: A very unique " << predAffiliation << std::endl;
-                            return 0;
-
-                        }
-                            
-
-                        
-                    }
-
-                    
-
-                }
-
-            }
+            std::cerr << "Oops, something went wrong creating the file.";
+            return 1;     
 
         } else {
-            std::cout << "Oops, your answer must be 1, 2, 3 or 4!" << std::endl;
+
+            // set fileContent to contents of the file
+            std::getline(file, fileContent);
+
+            // close file
+            file.close();
+
+            // get the prior probability
+            double priorProbability = stringToDouble(fileContent);
+            
+            // did we get any file content?
+            if (priorProbability < 0) {
+                std::cerr << "Error reading file: " << questionAffiliation << ".txt" << std::endl;
+                return 1;
+            } else {                    
+                
+                // let's play with Bayes' Theorem                    
+                double likelihood = 0.8;
+                double evidence = (likelihood * priorProbability) + ((1 - likelihood) * ( 1 - priorProbability));
+                double posteriorProbability = bayesTheorem(priorProbability, likelihood, evidence);
+
+                // reopen the file for writing
+                std::ofstream outputfile(questionAffiliation + ".txt");
+
+                // verify the file is open
+                if (!outputfile.is_open()) {
+                    std::cerr << "Error reopening file";
+                    return 1;
+                }
+
+                // write back to the file
+                outputfile << std::to_string(posteriorProbability) << std::endl;
+
+                // close the file
+                outputfile.close();
+
+                // we are comfortable enough to make a prediction
+                if(posteriorProbability > .8 && i > 3) {
+
+                    // if we already asked... don't ask again
+                    if(haveAsked != "true") {
+
+                        // we need to obtain the user's affiliation
+                        std::cout << "What number corresponds to your political affiliation?" << std::endl;
+                        std::cout << "Type the number of your choice and press enter:" << std::endl;
+                        std::cout << "1: Democrat" << std::endl;
+                        std::cout << "2: Republican" << std::endl;
+                        std::cout << "3: Green Party" << std::endl;
+                        std::cout << "4: Independent" << std::endl;
+                        std::cout << "Type the number of your choice and press enter:" << std::endl;
+                        std::cin >> userInputPoliticalParty;
+
+                        // validate user input
+                        int numberAff;
+                        while(isInputInt(userInputPoliticalParty, numberAff) != true) {
+                            std::cout << "Oops, your answer needs to be a number character 1-4." << std::endl;
+                            std::cout << "Type the number of your choice and press enter:" << std::endl;
+                            std::cin >> userInputPoliticalParty;
+                        }                       
+
+                    }       
+
+                    // convert user's number answer into the text equiv
+                    int adjAffiliateNumber = (stod(userInputPoliticalParty)) - 1;
+                    std::string affiliationMatch = affiliations[adjAffiliateNumber];
+
+                    // were we successful? if so... make prediction, If not... keep learing
+                    if (questionAffiliation == affiliationMatch) {
+                        
+                        std::cout << "Political Affiliation Prediction: " << questionAffiliation << std::endl;
+                        return 0;
+
+                    } else if (i == QuestionCount - 1) { // are we out of questions to ask?
+
+                        std::string predAffiliation = "";
+                        double prevProbability = 0;
+
+                        // if we didn't predict correctly, make the best guess we can
+                        for (int n = 0; n < affiliations.size(); ++n) {
+
+                            // open file
+                            std::string fContent;
+                            std::ifstream affiliationFile(affiliations[n] + ".txt");
+                            if (!affiliationFile.is_open()) {
+                                std::cerr << "Error opening file";
+                                return 1;
+                            } else {
+
+                                // get posterior probability
+                                std::getline(affiliationFile, fContent);
+
+                                if (n==0) {
+                                    predAffiliation = questionAffiliation;
+                                    prevProbability = std::stod(fContent);
+                                } else {
+                                    
+                                    if (std::stod(fContent) > prevProbability) {
+                                        // update the most li
+                                        predAffiliation = questionAffiliation;
+                                    }
+                                }
+
+                                // close file
+                                affiliationFile.close();
+
+                            }
+
+                        }
+
+                        // make our best guess
+                        std::cout << "Political Affiliation Prediction: A very unique " << predAffiliation << std::endl;
+                        return 0;
+
+                    }
+                        
+
+                    
+                }
+
+                
+
+            }
+
         }
+
 
     }
     return 0;
